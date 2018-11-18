99dd4f15f2764b8d7cd2ce90bfb4455809addefb
Krithika Vijayan <krithikavijayan@gmail.com>
2018-11-17 19:23:10 -0500

    Add files via upload

---
+++ packet.py
@@ -0,0 +1,9 @@
+# Packet definition
+
+class Packet(object):
+# Packet class contains the following:
+#	1.length of the packet 
+
+	def __init__(self):
+		self.packet_len = packet_len
+		self.protocol = protocol
