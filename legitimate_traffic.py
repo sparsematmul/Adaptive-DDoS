cbe11dd7c146e672b58a19b9d16dd64ef0d419b5
Krithika Vijayan <krithikavijayan@gmail.com>
2018-11-17 19:32:19 -0500

    Update legitimate_traffic.py

--- legitimate_traffic.py
+++ legitimate_traffic.py
@@ -4,3 +4,9 @@
 # this file will be used to have a notion of legitimate traffic
 # how much legtimate traffic is received at any given time
 # what is the latency observed and all those things will be modeled here
+import packet
+
+def FlowGenSimple(pkt_no):
+  for i in range(1,pkt_no):
+    sendtoNetwork(pkt)
+  
