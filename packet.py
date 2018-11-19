b9f223d94b5944e4b0207b22e608591a317cae87
Krithika Vijayan <krithikavijayan@gmail.com>
2018-11-19 02:58:04 -0500

    Update packet.py

--- packet.py
+++ packet.py
@@ -3,6 +3,8 @@
 class Packet(object):
 # Packet class contains the following:
 #	1.length of the packet 
+#	2.packet protocol - TCP/ UDP
+#	3.packet destination - which ingress location it does to 
 
 	def __init__(self):
 		self.packet_len = packet_len
