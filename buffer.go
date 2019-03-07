f28b56e64983cc33790e27855f0670924566b878
sparsematmul <44402616+sparsematmul@users.noreply.github.com>
2019-03-07 06:27:10 -0500

    Modified buffer and simulator to add for BACKLOG.
    Working on implementation

---
+++ buffer.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"math"
+	"time"
+	"fmt"
+)
+
+func enqueuePacket(pkt packet) {
+
+	LOCK_INGRESS_CAP[pkt.ingress].Lock()
+	if (INGRESS_CAP[pkt.ingress].availableBuffSpace - pkt.packet_len) > 0 {
+
+		// LOCK_RECEIVE_COUNTER[pkt.ingress].Lock()
+		// RECEIVE_COUNTER[pkt.ingress] +=1
+		// LOCK_RECEIVE_COUNTER[pkt.ingress].Unlock()
+
+		INGRESS_CAP[pkt.ingress].availableBuffSpace -= pkt.packet_len
+		// _DEBUG.Printf("Function: enqueuePacket - Packet Added to Queue, Available Buffer space at %d = %f", pkt.ingress, INGRESS_CAP[pkt.ingress].availableBuffSpace)
+
+		LOCK_INGRESS_CAP[pkt.ingress].Unlock()
+		diagnose(pkt)
+		// BUFFER[pkt.ingress].Add(pkt)
+		// BUFFER[pkt.ingress] <- pkt   // Send v to channel ch
+
+	} else {
+		LOCK_INGRESS_CAP[pkt.ingress].Unlock()
+		dropPacket(pkt)
+	}
+
+}
+
+func dropPacket(pkt packet) {
+
+	if pkt.attack_flag == false {
+
+		LOCK_legitimateDropCounter[pkt.ingress].Lock()
+		legitimateDropCounter[pkt.ingress] += 1
+		// _DEBUG.Printf("Function: dropPacket - Legitimate packet dropped, legitimateDropCounter = %d",legitimateDropCounter[pkt.ingress])
+		LOCK_legitimateDropCounter[pkt.ingress].Unlock()
+
+		//  	} else {
+		// 	attackDropCounter[pkt.ingress] +=1
+		//      // # _DEBUG.Printf(f"Function: dropPacket - Attack packet dropped, attackDropCounter = {attackDropCounter[pkt.ingress]}")
+	}
+}
+
+// func dequeuePackets(pktsToDequeue int, ingress int) {
+// 	for j := 0 ; j < pktsToDequeue ; j++ {
+// 				// pkt := <-BUFFER[i]
+// 		pkt,ok  := (BUFFER[ingress].Next()).(packet)
+// 		if(ok) {
+// 			// enqueuePacket(pkt)
+// 			diagnose(pkt)
+// 		}
+// 	}
+// }
+// func processing() {
+// 	for {
+// 		processPacket()
+// 		// time.Sleep(10 * time.Microsecond)
+// 	}
+// }
+
+func processPacket() {
+
+
+    if(BACKLOG==256)
+    {
+        fmt.Println("Backlog Full")  
+    }
+    s:=time.Now()
+
+    backlog[BACKLOG]=s.Add(time.Second*5)
+    BACKLOG+=1
+    
+    for i:=0; i< BACKLOG; i++{
+        if(time.Now().Equal(backlog[BACKLOG]))
+        {
+            for j:=i;j<BACKLOG-1;j++{
+                backlog[j]=backlog[j+1]
+            }
+            BACKLOG-=1
+        }
+    
+    }
+    
+
+	for i := 0; i < CONFIGURATION.INGRESS_LOC; i++ {
+		// # LOCK_RECEIVE_COUNTER[i].Lock()
+
+		// if(RECEIVE_COUNTER[i] > 0) {
+
+		LOCK_INGRESS_CAP[i].Lock()
+		pktsToDequeue := int(math.Ceil((INGRESS_CAP[i].vmQueue - INGRESS_CAP[i].availableBuffSpace) / PKT_LEN))
+		if pktsToDequeue > INGRESS_CAP[i].numOfDequeuePkts {
+			pktsToDequeue = INGRESS_CAP[i].numOfDequeuePkts
+		}
+		// if(float64(pktsToDequeue)*PKT_LEN > (INGRESS_CAP[i].cap - INGRESS_CAP[i].availableBuffSpace)) {
+
+		// }
+		// _DEBUG.Printf("Function: processPacket - %d packets to be processed, Available Buffer space at %d = %f", pktsToDequeue, i, INGRESS_CAP[i].availableBuffSpace)
+
+		INGRESS_CAP[i].availableBuffSpace += (PKT_LEN * float64(pktsToDequeue))
+
+		// _DEBUG.Printf("Function: processPacket - %d packets processed, Available Buffer space at %d = %f", pktsToDequeue, i, INGRESS_CAP[i].availableBuffSpace)
+		LOCK_INGRESS_CAP[i].Unlock()
+		// go dequeuePackets(pktsToDequeue,i)
+
+		// _DEBUG.Printf("Function: processPacket - after diagnose pkts to dequeue = %d", pktsToDequeue)
+
+		// }
+
+		// # LOCK_RECEIVE_COUNTER[i].Unlock()
+
+	}
+}
---
+++ simulator.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"os"
+	// "github.com/golang-collections/go-datastructures/fifo"
+	// "fmt"
+	"encoding/json"
+	"log"
+	"runtime"
+	"time"
+)
+
+var (
+	CONFIGURATION      Config
+	CURR_TRAFFIC_STATS []map[string]int
+	backlog [256]string
+	// PREV_TRAFFIC_STATS []map[string]int
+	PEAK_TRAFFIC []float64
+	MIN_TRAFFIC  []float64
+	AVG_TRAFFIC  []float64
+	// RECEIVE_COUNTER []int
+	legitimateDropCounter []int
+	// processCounter []int
+	// BUFFER []*fifo.Queue
+	NUM_VMs                 []int
+	INGRESS_CAP             []*VM
+	PKT_LEN                 float64 = 100.0 * 8 / 1000000
+	UDP_DETECT_ACCURACY     float64 = 0.9
+	TCP_SYN_DETECT_ACCURACY float64 = 0.9
+	DNS_AMP_DETECT_ACCURACY float64 = 0.9
+	EPOCH_TIME                      = 5.0
+	WINDOW_COUNTER          int
+    BACKLOG int = 0
+)
+
+func main() {
+
+	runtime.GOMAXPROCS(4)
+	if len(os.Args) < 2 {
+		log.Fatal("PLEASE ENTER FILENAME FOR CONFIG")
+	}
+	var configurationFile string = os.Args[1]
+	_DEBUG.Println("Hello from main")
+
+	_DEBUG.Println("Reading configuration file")
+
+	CONFIGURATION = LoadConfiguration(configurationFile)
+	s, _ := json.MarshalIndent(CONFIGURATION, "", "\t")
+	_DEBUG.Printf("%s", s)
+
+	_DEBUG.Printf("Initialize traffic stats data structures in ISP")
+	// initialize traffic stats data structures
+	initializeISP()
+
+	_DEBUG.Printf("Initialize capacity of ingress locations depending on the defense type")
+	initializeDefense()
+
+	initializeLocks()
+
+	// start legitimate traffic thread
+	_DEBUG.Printf("Start legitimate traffic thread")
+	for j := 0; j < CONFIGURATION.INGRESS_LOC; j++ {
+		go flowGenBenign("simple", j)
+	}
+	// start attack traffic thread
+	_DEBUG.Printf("Start attack traffic thread")
+	go attack()
+
+	// start dequeuing pkts after delay equivalent to processing delay
+	_DEBUG.Printf("Start packet processing thread - processing delay = %f s", CONFIGURATION.PROCESSING_DELAY)
+	_DEBUG.Printf("Start stats collection thread - epoch = %f s", EPOCH_TIME)
+	// stopProcess = Event()
+	// processingThread = RepeatingThread(stopProcess,PROCESSING_DELAY,"packet processingThread",buffer.processPacket)
+	// processingThread.start()
+	statsTicker := time.NewTicker(time.Duration(EPOCH_TIME) * time.Second)
+	processTicker := time.NewTicker(time.Duration(CONFIGURATION.PROCESSING_DELAY) * time.Millisecond)
+	// go processing()
+	// go flowGenBenign("simple", 0)
+	for {
+		select {
+		case <-processTicker.C:
+			go processPacket()
+		case <-statsTicker.C:
+			go collectStats()
+		}
+	}
+
+}
